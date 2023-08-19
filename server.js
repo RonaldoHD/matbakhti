1f01bfd1f4646f5be3a00ec01adb3775352110d0
RonaldoHD <ronaldohaddad2@gmail.com>
2023-08-19 17:28:41 +0300

    new update

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public')); // Serve files from the 'public' directory
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); // Serve your HTML file
+});
+
+
+app.get('/portfolio-details.html', (req, res) => {
+  const productName = req.query.product;
+
+  
+  // Now you can use the 'productName' variable to fetch details from your data
+  // and render the appropriate content
+  // ...
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
